22af5eb72a6b3e25df5916a9eac8b325d97afa7a
Deepak Gangwar <deeepakg1998@gmail.com>
2017-03-29 22:07:47 +0530

    basic framework ready for /grp_pro_assignment3/Q2

---
+++ grp_pro_assignment_3/Q2/Q2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdint.h>
+// #include <chrono>
+using namespace std;
+
+void Query(int64_t i,int64_t j,int64_t l,int64_t r,int64_t B[]){
+	int64_t sum = 0;
+	for (int64_t k = i; k <= j; k++)
+	{	
+		// cout<<"k: "<<k<<endl;
+		if((l <= B[k]) && (B[k] <= r))
+			{
+				sum = sum + B[k];
+				// cout<<"sum: "<<sum<<endl;
+			}
+	}
+	cout<<sum<<endl;
+}
+
+void Op(int64_t c[],int64_t b[],int64_t n){
+	int64_t zero[n/3],one[n/3],two[n/3];
+	for (int i = 0; i < n; ++i)
+	{
+		
+	}
+}
+
+int main() {
+	int64_t arraySize;
+	cin>>arraySize;
+	int64_t c[arraySize],b[arraySize];
+	for (int64_t i = 0;i < arraySize;i++){
+		cin>>c[i];
+	}
+	int64_t querySize;
+	cin>>querySize;
+	int64_t queries[4][querySize];
+	for (int64_t i = 0;i < querySize;i++){
+		cin>>queries[0][i]>>queries[1][i]>>queries[2][i]>>queries[3][i];
+		// cout<<queries[0][i]<<queries[1][i]<<queries[2][i]<<queries[3][i];
+	}
+
+
+	for (int64_t i = 0; i < querySize; i++)
+	{
+		// Op(c,b);
+		Query(queries[0][i],queries[1][i],queries[2][i],queries[3][i],c);	
+	}
+
+    return 0;
+}
