3d830799c7cd5121d3f63216ad6d6503712a08c8
Harsh Sinha <harshsin@iitk.ac.in>
2017-03-31 23:32:46 +0530

    added working code for merge sort

--- grp_pro_assignment_3/Q1/merge_sort.cxx
+++ grp_pro_assignment_3/Q1/merge_sort.cxx
@@ -1,44 +1,80 @@
+/*******************************
+ * Standard Naive Merge Sort
+ * ****************************/
+
+#include <ctime>
 #include <cstdlib>
 #include <iostream>
 
 typedef long long int llint_;
 
-#define ARRAY_SIZE      1e2
-#define AVERAGE_SIZE    1e2
+#define ARRAY_SIZE      int (1e2)
+#define INTEGER_SIZE    int (1e2)
 
 void merge_arrays (llint_ array[], int l, int r)
 {
     int mid     = l / 2 + r / 2;
-    int l_size  = mid - l;
-    int r_size  = r - mid + 1;
+    int l_size  = mid - l + 1;
+    int r_size  = r - mid;
+    int i = 0, j = 0;
     llint_ left_arr [l_size];
     llint_ right_arr [r_size];
 
-    for (int i = 0; i < l_size;  i ++) {
+    for (i = 0; i < l_size;  i ++) {
+
         left_arr [i] = array [l + i];
-    }
-    for (int i = 0; i < r_size;  i ++) {
-        right_arr [i] = array [i + mid];
+
     }
 
+    for (i = 0; i < r_size;  i ++) {
+
+        right_arr [i] = array [i + mid + 1];
+
+    }
+
+    i = 0, j = 0;
     while (i < l_size && j < r_size) {
-        :q
 
+        if (left_arr [i] < right_arr [j]) {
+
+            array [l + i + j] = left_arr [i];
+            i++;
+
+        }
+        else {
+
+            array [l + i + j] = right_arr [j];
+            j++;
+
+        }
+    }
+
+    while (i < l_size) {
+        array [l + i + j] = left_arr [i];
+        i++;
+    }
+
+    while (j < r_size) {
+        array [l + i + j] = right_arr [j];
+        j++;
     }
 }
 
-void merge_sort (int array[], int l, int r)
+void merge_sort (llint_ array[], int l, int r)
 {
-    if (l == r)
+    if (l >= r)
         return;
     else {
 
     int mid = (l / 2) + (r / 2);
 
-    merge_sort (array, l, mid - 1);
-    merge_sort (array, mid, r);
+//Sorting the left side of the array
+    merge_sort (array, l, mid);
 
-    merge_arrays (array, l, mid, r);
+//Sorting the right side of the array
+    merge_sort (array, mid + 1, r);
+
+    merge_arrays (array, l, r);
     }
 }
 
@@ -46,26 +82,34 @@ void merge_sort (int array[], int l, int r)
 void generate_array(llint_ * array, int array_len)
 {
 
+    std::srand (std::time (NULL));
+
     for (int i = 0; i < array_len; i++){
-        array[i] = (std::rand() % 100);
+        array[i] = (std::rand() % INTEGER_SIZE);
     }
 
 }
 
 void print_array (llint_ * array, int size)
 {
+
     for (int i = 0; i < size; i++){
+
         std::cout <<  array [i]  << '\n';
+
     }
+    std::cout << std::endl;
 }
 
 int main()
 {
-    int array_len = 10;
-    llint_ array[array_len];
-    generate_array (array, array_len);
 
-    merge_sort (array, 0, array_len - 1);
+    llint_ array[ARRAY_SIZE];
+    generate_array (array, ARRAY_SIZE);
+    print_array (array, ARRAY_SIZE);
 
-    print_array (array, 10);
+    merge_sort (array, 0, ARRAY_SIZE - 1);
+
+    print_array (array, ARRAY_SIZE);
+
 }
