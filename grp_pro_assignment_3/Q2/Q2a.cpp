fd67de9540c1ded293a6899a9837cb09676bf209
Deepak Gangwar <deeepakg1998@gmail.com>
2017-04-01 02:38:27 +0530

    Q2a done

--- grp_pro_assignment_3/Q2/Q2a.cpp
+++ grp_pro_assignment_3/Q2/Q2a.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
 #include <stdint.h>
-// #include <chrono>
-using namespace std;
 
-void Query(int64_t i,int64_t j,int64_t l,int64_t r,int64_t B[]){
-	int64_t sum = 0;
-	for (int64_t k = i; k <= j; k++)
-	{	
-		// cout<<"k: "<<k<<endl;
-		if((l <= B[k]) && (B[k] <= r))
-			{
-				sum = sum + B[k];
-				// cout<<"sum: "<<sum<<endl;
-			}
-	}
-	cout<<sum<<endl;
-}
+using namespace std;
 
 void Op(int64_t c[],int64_t low,int64_t high,int64_t offset){
 	int64_t n = high - low + 1;
-	// cout<<"C: ";
-	// for (int64_t i = low;i <= high;i++){
-	// 	cout<<c[i]<<"\t";
-	// }
-	// cout<<endl;
 	if(n <= 1) return;
 
 	int64_t n_two = n/3;
@@ -32,28 +13,19 @@ void Op(int64_t c[],int64_t low,int64_t high,int64_t offset){
 
 	if(n%3 == 1) n_zero += 1;
 	if(n%3 == 2) {n_zero +=1;n_one+=1;}
-	int64_t two[n_two],one[n_one],zero[n_one];
-	// int64_t *two = (int64_t*)calloc(n_two , sizeof(int64_t));
-	// int64_t *one = (int64_t*)calloc(n_one , sizeof(int64_t));
-	// int64_t *zero = (int64_t*)calloc(n_zero , sizeof(int64_t));
-	// cout<<"low: "<<low<<" high: "<<high<<" offset: "<<offset<<endl;
-	// cout<<"two: "<<two<<" one: "<<one<<" zero: "<<zero<<" offset: "<<offset<<endl;
-	// cout<<"n_two: "<<n_two<<" n_one: "<<n_one<<" n_zero: "<<n_zero<<" offset: "<<offset<<endl;
+	// int64_t two[n_two],one[n_one],zero[n_one];
+	int64_t *two = (int64_t*)calloc(n_two , sizeof(int64_t));
+	int64_t *one = (int64_t*)calloc(n_one , sizeof(int64_t));
+	int64_t *zero = (int64_t*)calloc(n_zero , sizeof(int64_t));
 	for (int64_t i = low; i <= high; i++){
 		if((i-low)%3 == 0)
-			{zero[(i-low)/3] = c[i];/*cout<<"zero: "<<zero[(i-low)/3]<<endl;*/}
+			zero[(i-low)/3] = c[i];
 		else if((i-low)%3 == 1)
-			{one[(i-low)/3] = c[i];/*cout<<"one: "<<one[(i-low)/3]<<endl;*/}
+			one[(i-low)/3] = c[i];
 		else if((i-low)%3 == 2)
-			{two[(i-low)/3] = c[i];/*cout<<"two: "<<two[(i-low)/3]<<endl;*/}
+			two[(i-low)/3] = c[i];
 	}
 
-	// free(two);
-	// free(one);
-	// free(zero);
-	Op(two,0,n_two-1,0);
-	Op(one,0,n_one-1,0);
-	Op(zero,0,n_zero-1,0);
 	for (int64_t i = low; i <= high; i++){
 		if((i-low) < n_two)
 			c[i] = two[i-low];
@@ -62,12 +34,19 @@ void Op(int64_t c[],int64_t low,int64_t high,int64_t offset){
 		if((i-low) < n_zero)
 			c[i+n_two+n_one] = zero[i-low];
 	}
+
+	free(two);
+	free(one);
+	free(zero);
+	Op(c,offset+0,offset+n_two-1,offset);
+	Op(c,offset+n_two,offset+n_one+n_two-1,offset+n_two);
+	Op(c,offset+n_one+n_two,offset+n_zero+n_one+n_two-1,offset+n_two+n_one);
 }
 
 int main() {
 	int64_t arraySize;
 	cin>>arraySize;
-	int64_t c[arraySize],b[arraySize];
+	int64_t c[arraySize],b[arraySize+1];
 	for (int64_t i = 0;i < arraySize;i++){
 		cin>>c[i];
 	}
@@ -77,22 +56,17 @@ int main() {
 	Op(c,0,arraySize-1,0);
 	for (int64_t i = 0;i < querySize;i++){
 		cin>>queries[0][i]>>queries[1][i]>>queries[2][i]>>queries[3][i];
-		// Query(queries[0][i],queries[1][i],queries[2][i],queries[3][i],c);	
-		// cout<<queries[0][i]<<queries[1][i]<<queries[2][i]<<queries[3][i];
 	}
 
-	// Op(c,0,arraySize-1,0);
+	b[0] = 0;
+	for(int64_t i = 0; i < arraySize;i++){
+		b[i+1] = b[i] + c[i];
+	}
+
 	for (int64_t i = 0; i < querySize; i++)
 	{
-		Query(queries[0][i],queries[1][i],queries[2][i],queries[3][i],c);	
+		cout<<b[queries[1][i]+1] - b[queries[0][i]]<<endl;	
 	}
-
-	// Op(c,b,0,arraySize-1,0);
-	// for (int64_t i = 0;i < arraySize;i++){
-	// 	cout<<b[i]<<"\t";
-	// }
 	
     return 0;
 }
-
-//19111800	2017-03-30 18:07:11	swatigupta1997	Sub array Sum1	accepted	1.99	4.7M	C++ 4.3.2
